723781ed9e1cd5ffe2497fd0273ad98a7c60f4aa
Iago Vinícius Souza de Sales <pristonvinicius@hotmail.com>
2025-12-11 19:42:57 -0300

    feat: add menu app with index view and template

--- menu/views.py
+++ menu/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
 
-# Create your views here.
+from .models import Dish, Cook, DishType, Ingredient
+
+def index(request):
+    num_dishes = Dish.objects.count()
+    num_cooks = Cook.objects.count()
+    num_dish_types = DishType.objects.count()
+    num_ingredients = Ingredient.objects.count()
+    context = {
+        "num_dishes": num_dishes,
+        "num_cooks": num_cooks,
+        "num_dish_types": num_dish_types,
+        "num_ingredients": num_ingredients,
+    }
+    return render(request, "menu/index.html", context=context)
