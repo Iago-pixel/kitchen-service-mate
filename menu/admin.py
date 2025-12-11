0bacc69f37ed49093a2c48d676e457b2a21546de
Iago Vinícius Souza de Sales <pristonvinicius@hotmail.com>
2025-12-11 15:30:05 -0300

    feat: add models in admin

--- menu/admin.py
+++ menu/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+from .models import Dish, DishType, Ingredient, Cook
+
+admin.site.register(Cook, UserAdmin)
+admin.site.register(DishType)
+admin.site.register(Ingredient)
+admin.site.register(Dish)
