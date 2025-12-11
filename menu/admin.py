66561f080982c4eec80cb3d033ccc85de09862a7
Iago Vinícius Souza de Sales <pristonvinicius@hotmail.com>
2025-12-11 15:49:45 -0300

    make migrations to add default value in years_of_experience

--- menu/admin.py
+++ menu/admin.py
@@ -3,7 +3,13 @@ from django.contrib.auth.admin import UserAdmin
 
 from .models import Dish, DishType, Ingredient, Cook
 
+
+@admin.register(Dish)
+class DishAdmin(admin.ModelAdmin):
+    list_display = ("name", "price", "dish_type")
+    list_filter = ("dish_type", )
+    search_fields = ("name", )
+
 admin.site.register(Cook, UserAdmin)
 admin.site.register(DishType)
 admin.site.register(Ingredient)
-admin.site.register(Dish)
