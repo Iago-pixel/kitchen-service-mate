b874c10f9351e9a3d32881b3954f05d89206e8b0
Iago Vinícius Souza de Sales <pristonvinicius@hotmail.com>
2025-12-12 19:38:49 -0300

    feat: add Django Debug Toolbar for improved debugging experience

--- app/settings.py
+++ app/settings.py
@@ -37,11 +37,13 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "debug_toolbar",
     "menu",
 ]
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
+    "debug_toolbar.middleware.DebugToolbarMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
@@ -121,4 +123,8 @@ AUTH_USER_MODEL = "menu.Cook"
 
 STATICFILES_DIRS = [
     BASE_DIR / "static",
+]
+
+INTERNAL_IPS = [
+    "127.0.0.1",
 ]
