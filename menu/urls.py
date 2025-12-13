e15468ca98bb3c012864387b1f5269ff717d5b21
Iago Vinícius Souza de Sales <pristonvinicius@hotmail.com>
2025-12-13 18:49:53 -0300

    refactor: clean up code formatting and improve readability in admin, models, urls, and views

--- menu/urls.py
+++ menu/urls.py
@@ -1,15 +1,15 @@
 from django.urls import path
 
 from .views import (
-  index,
-  DishTypeListView,
-  IngredientListView,
-  DishListView,
-  DishDetailView,
-  CookListView,
-  CookDetailView,
-  DishTypeCreateView,
-  IngredientCreateView,
+    index,
+    DishTypeListView,
+    IngredientListView,
+    DishListView,
+    DishDetailView,
+    CookListView,
+    CookDetailView,
+    DishTypeCreateView,
+    IngredientCreateView,
 )
 
 urlpatterns = [
@@ -17,7 +17,9 @@ urlpatterns = [
     path("dish-types/", DishTypeListView.as_view(), name="dish-type-list"),
     path("dish-types/create/", DishTypeCreateView.as_view(), name="dish-type-create"),
     path("ingredients/", IngredientListView.as_view(), name="ingredient-list"),
-    path("ingredients/create", IngredientCreateView.as_view(), name="ingredient-create"),
+    path(
+        "ingredients/create", IngredientCreateView.as_view(), name="ingredient-create"
+    ),
     path("dishes/", DishListView.as_view(), name="dish-list"),
     path("dishes/<int:pk>", DishDetailView.as_view(), name="dish-detail"),
     path("cooks/", CookListView.as_view(), name="cook-list"),
--- menu/views.py
+++ menu/views.py
@@ -6,6 +6,7 @@ from django.urls import reverse_lazy
 
 from .models import Dish, Cook, DishType, Ingredient
 
+
 @login_required
 def index(request):
     num_dishes = Dish.objects.count()
