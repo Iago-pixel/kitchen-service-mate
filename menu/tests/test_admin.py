2f246c3199e57b844b8c9cc9b5826d9bb7d64802
Iago Vinícius Souza de Sales <pristonvinicius@hotmail.com>
2025-12-19 15:39:24 -0300

    refactor: remove unnecessary Client instantiation in admin site tests

--- menu/tests/test_admin.py
+++ menu/tests/test_admin.py
@@ -1,10 +1,9 @@
-from django.test import TestCase, Client
+from django.test import TestCase
 from django.contrib.auth import get_user_model
 from django.urls import reverse
 
 class AdminSiteTests(TestCase):
     def setUp(self):
-        self.client = Client()
         self.admin_user = get_user_model().objects.create_superuser(
             username="admin",
             password="testadmin"
